8b5d4fc42e32cf4d64dcbe04cea7f9ebda9abebf
pier <501311328@qq.com>
2018-02-13 00:26:36 +0800

    String 0

---
+++ src/main/java/com/pier/algorithm/chap5_string/IsDeformation.java
@@ -0,0 +1,34 @@
+package com.pier.algorithm.chap5_string;
+
+/**
+ * @author: pier
+ * @date: 2018/2/12 23:38
+ * @desc: 判断两个字符串是否互为变形词
+ */
+
+public class IsDeformation {
+    public static boolean isDeformation(String str1, String str2) {
+        if (str1 == null || str2 == null || str1.length() != str2.length()) {
+            return false;
+        }
+        char[] chas1 = str1.toCharArray();
+        char[] chas2 = str2.toCharArray();
+        int[] map = new int[256];
+        for (int i = 0; i < chas1.length; i++) {
+            map[chas1[i]]++;
+        }
+        for (int i = 0; i < chas2.length; i++) {
+            if (map[chas2[i]]-- == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String A = "abcabcabc";
+        String B = "bcacbaacb";
+        System.out.println(isDeformation(A, B));
+
+    }
+}
---
+++ src/main/java/com/pier/algorithm/chap5_string/RemoveKZeros.java
@@ -0,0 +1,11 @@
+package com.pier.algorithm.chap5_string;
+
+/**
+ * @author: pier
+ * @date: 2018/2/13 0:15
+ * @desc:
+ */
+
+public class RemoveKZeros {
+    
+}
